46148e0c265109a20ebc22f95d8ebf98087fef71
kikoralston <fralston@andrew.cmu.edu>
2016-03-13 11:54:59 -0400

    Added alternative 2 to R codes and created report.tex

--- R/define_alternatives.R
+++ R/define_alternatives.R
@@ -1,13 +1,13 @@
 source("EngEcon.R")
 
 compute.congestion.cost.minute <- function(congestion.costs) {
-  # Computes congestion cost per minute from commuter data
+  # Computes congestion cost per minute using commuter data
   return(congestion.costs$annual.cost.per.commuter/
            (congestion.costs$annual.hours.per.commuter*60))
 }
 
 compute.congestion.costs <- function(parameters, congestion.costs, 
-                                    transit.data) {
+                                     transit.data) {
   # Computes annual congestion cost
   
   # computes congestion cost per minute
@@ -21,12 +21,16 @@ compute.congestion.costs <- function(parameters, congestion.costs,
 }
 
 compute.mortality.costs <- function(parameters, transit.risks){
+  # Computes annual mortality cost
+  
   annual.cost.mortality <- parameters$vsl * sum(transit.risks$victims$fatality)
   cost.mortality <- rep(annual.cost.mortality, parameters$num.years)
   return(cost.mortality)
 }
 
 compute.injury.costs <- function(parameters, transit.risks, injury.costs){
+  # Computes annual injury cost
+  
   inj.victims <- transit.risks$victims$injuries
   names(inj.victims) <- transit.risks$victims$type
   inj.costs <- unlist(injury.costs)
@@ -60,7 +64,7 @@ compute.pollution.costs <- function(parameters, transit.data, bus.emission,
   # IMPORTANT: I ASSUMED THAT ONLY 10% OF TRAVEL TIME IS IDLE TIME
   # CHECK THIS ASSUMPTION
   annual.pollution.cost <- bus.miles.travel.annual*cost.per.mile+
-    bus.total.time.annual*cost.per.idle.minute*0.1
+    bus.total.time.annual*cost.per.idle.minute*parameters$share.idle.time
   
   return(rep(annual.pollution.cost, parameters$num.years))
 }
@@ -88,12 +92,53 @@ compute.ghg.costs <- function(parameters, transit.data, bus.emission,
   # IMPORTANT: I ASSUMED THAT ONLY 10% OF TRAVEL TIME IS IDLE TIME
   # CHECK THIS ASSUMPTION
   annual.ghg.cost <- bus.miles.travel.annual*cost.per.mile+
-    bus.total.time.annual*cost.per.idle.minute*0.1
+    bus.total.time.annual*cost.per.idle.minute*parameters$share.idle.time
   
   return(rep(annual.ghg.cost, parameters$num.years))
 }
 
+compute.AM.capital.costs <- function(parameters, buses.data, costs.am) {
+  # Compute capital costs of implementing AM
+  
+  # capital costs are only incurred in the first year
+  
+  # Summing different costs for each age of bus
+  cap.cost.am <- sum(buses.data$number * 
+    c(costs.am$cc.old.bus, costs.am$cc.old.bus, costs.am$cc.old.bus))
+  
+  # Adding cost of simulator facility
+  cap.cost.am <- cap.cost.am + costs.am$simulator
+  
+  return(c(cap.cost.am, rep(0, (parameters$num.years-1))))
+}
 
+compute.AM.oem.costs <- function(parameters, buses.data, costs.am) {
+  # Compute OeM costs of AM
+  
+  n.years.before.am <- parameters$year.AV - parameters$year.ini
+  
+  # oem
+  oem.cost.am <- sum(buses.data$number) * costs.am$oemc 
+  
+  # expected overhaul costs
+  n.overhaul.cases <- length(costs.am$overhaul$years)
+  overhaul.costs <- rep(0, parameters$num.years)
+  for (i in 1:n.overhaul.cases) {
+    year.overhaul <- n.years.before.am + costs.am$overhaul$years[i]
+    overhaul.costs[year.overhaul] <- overhaul.costs[year.overhaul] + 
+      costs.am$overhaul$prob[i] * costs.am$overhaul.cost * 
+      sum(buses.data$number)
+  }
+  
+  # create vector of OeM costs
+  oem.cost.am <- c(rep(0, (n.years.before.am)),
+                   rep(oem.cost.am, parameters$num.years - n.years.before.am))
+  
+  # include expected overhaul cost
+  oem.cost.am <- oem.cost.am + overhaul.costs
+  
+  return(oem.cost.am)
+}
 
 alternative1 <- function(parameters, buses.data, transit.risks, 
                          bus.emission, emission.costs, injury.costs,
@@ -123,4 +168,111 @@ alternative1 <- function(parameters, buses.data, transit.risks,
   
   # return list with flow of costs
   return(bc.flow)
+}
+
+alternative2 <- function(parameters, buses.data, transit.risks, 
+                         bus.emission, emission.costs, injury.costs,
+                         transit.data, congestion.costs, costs.am){
+  bc.flow <- list()
+
+  # AM capital costs
+  bc.flow$cost.AM.capital <- compute.AM.capital.costs(parameters, 
+                                                      buses.data, 
+                                                      costs.am)
+
+  # AM OeM costs
+  bc.flow$cost.AM.oem <- compute.AM.oem.costs(parameters, 
+                                              buses.data, 
+                                              costs.am)
+  
+  # mortality
+  bc.flow$cost.mortality <- compute.mortality.costs(parameters, transit.risks)
+  
+  # injuries
+  bc.flow$cost.injury <- compute.injury.costs(parameters, transit.risks, 
+                                              injury.costs)
+  
+  # Traffic Congestion
+  bc.flow$cost.congestion <- compute.congestion.costs(parameters, 
+                                                      congestion.costs, 
+                                                      transit.data)
+  # Air pollutants
+  bc.flow$cost.air.pollution <- compute.pollution.costs(parameters, 
+                                                        transit.data, 
+                                                        bus.emission,
+                                                        emission.costs)
+  
+  # GHG
+  bc.flow$cost.ghg <- compute.ghg.costs(parameters,transit.data,
+                                        bus.emission,emission.costs)
+  
+  # return list with flow of costs
+  return(bc.flow)
+}
+
+exp.alternative2 <- function(parameters, buses.data, transit.risks, 
+                             bus.emission, emission.costs, injury.costs,
+                             transit.data, congestion.costs, costs.am,
+                             change.commute){
+  # computes expected cash flow for alternative 2
+  # takes into account uncertainty in congestion and safety
+  
+  # reads data of fatalities and injury change
+  fatality.change <- read.csv(file= "../../csvfiles/fatalityreduction.csv")
+  injury.change <- read.csv(file= "../../csvfiles/injuriesreduction.csv")
+  
+  # exclude data from interview #5 because it was too different from the rest
+  fatality.change <- fatality.change[1:4, ]
+  injury.change <- injury.change[1:4, ]
+  
+  # computes mean of 4 remaining interviews
+  fatality.change.mean <- colMeans(fatality.change)
+  injury.change.mean <- colMeans(injury.change)
+  
+  # probabilities of each case (guessed by us)
+  prob.safety <- c(0.1, 0.8, 0.1)
+  
+  # computes \sum_i \sum_j CashFlow_{i,j} * Prob_{i,j}
+  # assumes SAFETY and CONGESTION are independents
+  for (i in 1:3) {
+    # compute cash flow for each case of change in CONGESTION
+    
+    # change travel time
+    transit.data.am <- transit.data
+    transit.data.am$Average.time.in.daily.person.trip.minutes <-
+      transit.data.am$Average.time.in.daily.person.trip.minutes + 
+      change.commute$time[i]/60
+    
+    for (j in 1:3) {
+      # compute cash flow for each case of change in SAFETY
+      # (we assume that fatality and injury are the same event)
+      
+      # change transit risks
+      transit.risks.am <- transit.risks
+      transit.risks.am$victims$fatality <- transit.risks.am$victims$fatality*
+        (1-fatality.change.mean[j])
+      transit.risks.am$victims$injuries <- transit.risks.am$victims$injuries*
+        (1-injury.change.mean[j])
+      
+      cf.case <- alternative2(parameters, buses.data, transit.risks.am, 
+                              bus.emission, emission.costs, injury.costs,
+                              transit.data.am, congestion.costs, costs.am)
+      
+      if (i == 1 && j == 1) {
+        # first case: initialize list with cf*probability
+        p <- prob.safety[j]*change.commute$prob[i]
+        cf.final <- mapply("*", cf.case , list(p), SIMPLIFY = FALSE)
+      } else{
+        # other cases: sum with previous cases
+        p <- prob.safety[j]*change.commute$prob[i]
+        cf.final <- mapply(function(x, y, wx, wy) wx*x+wy*y, 
+                           cf.final, cf.case, 
+                           MoreArgs = list(wx=1, wy=p), SIMPLIFY = FALSE)
+        
+      }
+    }
+  }
+  
+  # returns final expected cash flow
+  return(cf.final)
 }
--- R/project_main.R
+++ R/project_main.R
@@ -6,7 +6,7 @@ source("define_alternatives.R")
 # -------------------------------------------------
 # Creates lists of parameters, costs and other inputs
 
-# parameters
+# **parameters**
 parameters <- list()
 parameters$disc.rate <- 0.05 # discount rate (% p.y.)
 parameters$num.years <- 30 # number of years of analysis
@@ -14,8 +14,10 @@ parameters$year.ini <- 2016 # initial year of cash flow
 parameters$year.AV <- 2020 # initial year of AV implementation
 parameters$ny.population <- 8.4e6 # population
 parameters$vsl <- 9.2e6 # value of statistical life ($)
+# share of time that vehicle is idle (our assumption)
+parameters$share.idle.time <- 0.1 
 
-# injury costs. 
+# **injury costs**
 # source: https://wisqars.cdc.gov:8443/costT/cost_Part1_Intro.jsp
 injury.costs <- list()
 injury.costs$occupant <- 173711
@@ -24,11 +26,11 @@ injury.costs$pedalciclyst <- 187807
 injury.costs$pedestrian <- 191813
 injury.costs$other <- 139217
 
-# number of buses in NYC segmented by age (source: memo)
+# **number of buses in NYC segmented by age (source: memo)**
 buses.data <- data.frame(age = c(4, 9, 20), 
                          number = c(2313, 1296, 1437))
 
-# costs of automerge (source: memo)
+# **costs of automerge (source: memo)**
 costs.am <- list()
 costs.am$cc.new.bus <- 5000 # cost to install in new bus (<= 4)
 costs.am$cc.med.bus <- 6500 # cost to install in medium aged bus (5-9)
@@ -40,13 +42,13 @@ costs.am$overhaul <- data.frame(years=c(4, 5, 6),
 costs.am$overhaul.cost <- 3000 # overhaul cost
 costs.am$simulator <- 2.8e6 # simulator facility
 
-# Changes in commute times with EM (source: memo)
+# **Changes in commute times with EM (source: memo)**
 change.commute <- data.frame(outcome = c("MB", "LB", "LW"),
                              time = c(-235, -55, 30),
                              margin.error = c(20, 15, 10),
                              prob = c(0.2, 0.4, 0.4))
 
-# transit risks (source:memo)
+# **transit risks (source:memo)**
 transit.risks <- list()
 transit.risks$daily.trips <- 1.52e6
 transit.risks$trip.avg.time <- 49
@@ -57,8 +59,8 @@ transit.risks$victims <- data.frame(type = c("motorist", "passenger",
                                     fatality = c(3, 1.5, 0.5, 11),
                                     injuries = c(446, 837, 67, 390))
 
-# bus gas emissions (source:  Average In-Use Emissions from Urban Buses 
-#                             and School Buses)
+# **bus gas emissions**
+# (source:  Average In-Use Emissions from Urban Buses and School Buses)
 bus.emission <- list()
 bus.emission$nox <- 17.2 # NOx emission (g/mi)
 bus.emission$pm <- 0.172 # particulate emission (g/mi)
@@ -76,7 +78,7 @@ bus.emission$ghg.idle <- 0 # GHG idle emission (g/min)
 easiur <- read.csv(file= "../../csvfiles/easiur_nyclocation.csv")
 easiur.annual <- easiur[ ,grep("Annual.Ground", names(easiur))]
 
-# emission costs
+# **emission costs**
 # sources:  http://www3.epa.gov/climatechange/EPAactivities/economics/scc.html 
 #           http://barney.ce.cmu.edu/~jinhyok/easiur/online/
 emission.costs <- list()
@@ -91,43 +93,61 @@ emission.costs$nox <- easiur.annual$NOX.Annual.Ground
 # social cost of NH3 ($/ton)
 emission.costs$nh3 <- easiur.annual$NH3.Annual.Ground 
 
-# congestion costs (source: page 8 in memo)
+# **congestion costs (source: page 8 in memo)**
 congestion.costs <- list()
 congestion.costs$annual.hours.per.commuter <- 38 # hours
 congestion.costs$annual.cost.per.commuter <- 818 # ($)
 congestion.costs$cost.per.hour <- 22 # $/hour for each commuter
 
-# NYC transit data
+# **NYC transit data**
 transit.data <- read.csv(file= "../../csvfiles/urbantranspsystem.csv")
 
 # -------------------------------------------------
 # Analysis
+
+order.levels <- c("cost.AM.capital", "cost.AM.oem", "cost.mortality", 
+                  "cost.injury", "cost.congestion", "cost.air.pollution",
+                  "cost.ghg" )
 
 # option 1 (do nothing)
 cf.option1 <- alternative1(parameters, buses.data, transit.risks, 
                            bus.emission, emission.costs, injury.costs,
                            transit.data, congestion.costs)
 
-npv.costs <- as.data.frame(lapply(cf.option1, FUN = npv, 
+npv.costs1 <- as.data.frame(lapply(cf.option1, FUN = npv, 
                                   rate = parameters$disc.rate))/1e9
-npv.costs <- gather(npv.costs, type, value)
-npv.costs$option <- rep(c("alternative 1"), nrow(npv.costs)) 
-npv.costs$type <- factor(npv.costs$type, levels=npv.costs$type)
+npv.costs1 <- gather(npv.costs1, type, value)
+npv.costs1$option <- rep(c("alternative 1"), nrow(npv.costs1)) 
+npv.costs1$type <- factor(npv.costs1$type, levels=order.levels)
 
 # option 2 (perform test)
+cf.option2 <- exp.alternative2(parameters, buses.data, transit.risks, 
+                               bus.emission, emission.costs, injury.costs,
+                               transit.data, congestion.costs, costs.am, 
+                               change.commute)
+
+npv.costs2 <- as.data.frame(lapply(cf.option2, FUN = npv, 
+                                   rate = parameters$disc.rate))/1e9
+npv.costs2 <- gather(npv.costs2, type, value)
+npv.costs2$option <- rep(c("alternative 2"), nrow(npv.costs2)) 
+npv.costs2$type <- factor(npv.costs2$type, levels=order.levels)
 
 # option 3 (implement AV without testing)
 
 # plots stacked bar plot with results
 
+npv.costs <- rbind(npv.costs1, npv.costs2)
+
 g <- ggplot() + geom_bar(data = npv.costs, 
                          aes(x=option, y=value, fill=type), 
                          stat = "identity", width = 0.2) + 
   theme_bw(base_size = 16) + ylab("NPV ($ Billion)") +
-  theme(axis.title.x = element_blank(), 
-        legend.position=c(1,1), legend.justification=c(1,1)) +
+  theme(axis.title.x = element_blank())+ 
+        # legend.position=c(1,1), legend.justification=c(1,1)) +
   guides(fill=guide_legend(title=NULL, reverse = TRUE)) +
-  scale_fill_grey() + geom_hline(yintercept = 0)
+  scale_fill_grey() + geom_hline(yintercept = 0) + 
+  scale_y_continuous(breaks = seq(0, 32, by=2)) + 
+  coord_cartesian(ylim = c(20, 32))
 
 # width and height are in pixels
 png("barplot1.png", width=480, height = 480)
